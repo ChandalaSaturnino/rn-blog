27996cc346054cb3da5c6a8aa2ed98d13926b531
Jose Heon Gaza Floes <chandala@Joses-MacBook-Pro.local>
2022-08-12 13:58:36 -0500

    blog react native app first commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Index</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default IndexScreen;
